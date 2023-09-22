ee0f5f7d4eebb9ce4baa276978552a4afbc606c4
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2023-09-21 23:46:29 -0300

    Update database setup

--- mongo/setup-database.js
+++ mongo/setup-database.js
@@ -6,4 +6,6 @@ db.createUser({
   roles: [{ role: 'readWrite', db: process.env.MONGO_INITDB_ROOT_DATABASE }],
 });
 
-db.createCollection('people')
+db.createCollection('users')
+db.createCollection('user_chat')
+db.createCollection('user_profile')
